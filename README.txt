b988799d686643a4eae686e52556499635a72705
Sam Hennessey <sml18vly@bangor.ac.uk>
2022-04-06 10:49:24 +0100

    remove data; retrieve automatically from Gurenberg instead

--- README.txt
+++ README.txt
@@ -1,6 +1,10 @@
 zipf analysis
 =============
 
+This script will automatically pull the full text of the two books to
+process (Frankenstein and Dracula) from Project Gutenberg (gutenberg.org) and place
+them into the `data` directory. Internet access is required for this to work.≈
+
 This repository contains code to observe whether books adhere to Zipf's law, as
 done in support of the paper "Zipf analysis of 19th-century English-language books",
 V. Dracula, to appear in Annals of Computational Linguistics, 2022.
--- bin/run_analysis.sh
+++ bin/run_analysis.sh
@@ -1,6 +1,10 @@
 mkdir results
+mkdir data
+curl -L -o data/frankenstein.txt https://www.gutenberg.org/files/84/84-0.txt
+curl -L -o data/dracula.txt https://www.gutenberg.org/files/345/345-0.txt
+
 for book in dracula frankenstein
 do
-    python bin/countwords.py data/${book}.txt --num 100 > results/${book}.csv
-    python bin/plotcounts.py results/${book}.csv --outfile results/${book}.pdf
+    python3 bin/countwords.py data/${book}.txt --num 100 > results/${book}.csv
+    python3 bin/plotcounts.py results/${book}.csv --outfile results/${book}.pdf
 done
